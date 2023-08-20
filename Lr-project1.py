ccfdca09ae29e165b714dcc7d4c9f4036960ae4b
Rvaibhavv <107602634+Rvaibhavv@users.noreply.github.com>
2023-08-20 14:14:15 +0530

    Update Lr-project1.py

--- Lr-project1.py
+++ Lr-project1.py
@@ -6,7 +6,7 @@ from sklearn.utils import shuffle
 import sklearn
 
 
-data=pd.read_csv("/content/student-mat.csv",sep=";")
+data=pd.read_csv("student-mat.csv",sep=";")
 data=data[["G1","G2","G3","studytime","failures","absences"]]
 print(data.head())
 
